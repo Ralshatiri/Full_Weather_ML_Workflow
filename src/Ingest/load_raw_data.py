2746c72bed652103ba31b51b639437a073dabcf4
rahaf alshatiri <rahaf.alshatri14@gmail.com>
2026-07-02 23:32:34 +0300

    update paths to read from s3

--- src/Ingest/load_raw_data.py
+++ src/Ingest/load_raw_data.py
@@ -2,18 +2,35 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 from sqlalchemy import create_engine, text
-from src.config import CSV_PATH , DB_CONN
+from src.config import  DB_CONN
+from io import BytesIO
+import boto3
 
 load_dotenv()
 
 
 
+def read_raw_weather_csv_from_s3() -> pd.DataFrame:
+    bucket = os.getenv("MODEL_S3_BUCKET")
+    key = os.getenv("RAW_DATA_S3_KEY")
+
+    if not bucket:
+        raise ValueError("MODEL_S3_BUCKET is not set")
+
+    if not key:
+        raise ValueError("RAW_DATA_S3_KEY is not set")
+
+    s3 = boto3.client("s3")
+    response = s3.get_object(Bucket=bucket, Key=key)
+
+    return pd.read_csv(BytesIO(response["Body"].read()))
 
 def ingest():
 
     try:
         print('Reading data from csv.........')
-        data = pd.read_csv(CSV_PATH)
+        data = read_raw_weather_csv_from_s3()
+
         print(f"Loaded {len(data)} rows")
 
         engine = create_engine(DB_CONN)
